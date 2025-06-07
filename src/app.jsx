3dc770ff676804ab00bd4f0f7b678c22db9020ee
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-06-07 10:29:05 +0200

    add useEffect to App

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
 import { LoadingPage } from './pages/LoadingPage/LoadingPage';
 import { HomePage } from './pages/HomePage/HomePage';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
+  const [books, setBooks] = useState([]);
+  //const [,] = useState('');
+
+  useEffect(() => {
+    if (!books) setItems([]);
+    else {
+      const fetchBooks = async () => {
+        const response = await fetch(`http://localhost:4000/api/books`);
+        const json = await response.json();
+        setBooks(json.data);
+      };
+
+      fetchBooks();
+    }
+  }, []);
+
   return (
     <>
       <HomePage />
