385e66ed106b51f8ad4140b195a4081c879a312f
Ivana Janoštíková <ivkagre@gmail.com>
2025-06-08 22:02:25 +0200

    adjusted SHELFIE logo in Header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import { AddBookForm } from '../../components/AddBookForm/AddBookForm';
 import { Footer } from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
-import { LoadingPage } from '../LoadingPage/LoadingPage';
+
 import './HomePage.css';
 import { Outlet } from 'react-router-dom';
 
