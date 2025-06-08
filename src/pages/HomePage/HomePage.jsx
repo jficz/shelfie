c6ca0cf2708ecfe26a05464ddcdfabe696490204
Ivana Janoštíková <ivkagre@gmail.com>
2025-06-08 19:43:56 +0200

    Header style fixed, footer fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,11 @@
 import { AddBookForm } from '../../components/AddBookForm/AddBookForm';
 import { Footer } from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
+import { LoadingPage } from '../LoadingPage/LoadingPage';
 import './HomePage.css';
 import { Outlet } from 'react-router-dom';
+
+
 
 export const HomePage = () => {
   return (
