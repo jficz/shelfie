8beef2eeba2f7987619f6eba195b705dc8617878
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-06-10 17:51:43 +0200

    Move assets to public

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -15,30 +15,30 @@ export const Menu = ({ onLink }) => {
         </li>
         <li className="list-item">
           <button className="list-button">
-            <img src="../assets/icons/scan.png" alt="sken isbn" /> Scan ISBN
+            <img src="assets/icons/scan.png" alt="sken isbn" /> Scan ISBN
           </button>
         </li>
         <li className="list-item">
           <button className="list-button">
-            <img src="../assets/icons/search.png" alt="vyhledat v antikvariátu" />
+            <img src="assets/icons/search.png" alt="vyhledat v antikvariátu" />
             Antikvariát
           </button>
         </li>
         <li className="list-item">
           <button className="list-button">
-            <img src="../assets/icons/filter.png" alt="filtrovat" />
+            <img src="assets/icons/filter.png" alt="filtrovat" />
             Filtrovat
           </button>
         </li>
         <li className="list-item">
           <button className="list-button">
-            <img src="../assets/icons/statistic.png" alt="statistiky" />
+            <img src="assets/icons/statistic.png" alt="statistiky" />
             Statistiky
           </button>
         </li>
         <li className="list-item">
           <button className="list-button">
-            <img src="../assets/icons/settings.png" alt="nastavení" />
+            <img src="assets/icons/settings.png" alt="nastavení" />
             Nastavení
           </button>
         </li>
