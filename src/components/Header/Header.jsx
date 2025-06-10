c9d7e7ba608d160023738504f80fd9d902eda687
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-06-10 12:06:43 +0200

    Bad commit, 2nd attempt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -49,7 +49,8 @@ export const Header = () => {
         <Link to={'/'}>
           <img className="logo-header-small" src="assets/logo_small.png"></img>
         </Link>
-        <div className="only-desktop">SHELFIE</div>
+        <div className="header-title">SHELFIE</div>
+        <div className="header-shelf"></div>
       </header>
       {menuOpened ? (
         <div ref={menuRef}>
@@ -59,25 +60,3 @@ export const Header = () => {
     </>
   );
 };
-
-//media header
-// export const Header = () => {
-//   return (
-//     <>
-//       {/* <!--Header--> /}
-//       <header className="header-big">
-//         {/ <!-- Navigation -->  /}
-//         <div className="header-title">Shelfie</div>
-
-//           {/ <!-- Home-Page -->
-
-//           <div className="blabla">
-//           <a href="index.html" className="nav-icon nav-icon--home">
-//             <img src="assets/icons/home_icon.svg" alt="Home" />
-//           </a>
-
-//        </div>*/}
-//       </header>
-//     </>
-//   );
-// };
